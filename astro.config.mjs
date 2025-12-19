0b1bd27afc8d2d92306b6bf8ba23f1c82ba5961c
らいとん <raitosongwei@gmail.com>
2025-12-20 04:59:48 +0900

    chore: パッケージのインストール、パスのエイリアスの追加、Astroの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,22 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://raiton-boo.github.io',
+  base: '/synchro-site', //リポジトリ名
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src',
+        '@lib': '/src/lib',
+        '@core': '/src/lib/core',
+        '@sync': '/src/lib/sync',
+        '@interaction': '/src/lib/interaction',
+        '@audio': '/src/lib/audio',
+        '@debug': '/src/lib/debug',
+        '@utils': '/src/lib/utils',
+        '@types': '/src/lib/types',
+      },
+    },
+  },
+});
